2d6baa7d34303eae9527732210d9749843a31d32
Morinhugo <morinhugo92@gmail.com>
2021-07-14 06:14:07 -0300

    Exercicio 1 Aula ao vivo Prof Lucas

---
+++ app.js
@@ -0,0 +1,13 @@
+let Nota1 = prompt("Digite a primeira Nota: ");
+let Nota2 = prompt("Digite) a segunda Nota: ");
+let Nota3 = prompt("Digite a Terciera Nota ");
+
+
+let Result1 = (Nota1 + Nota2 + Nota3) / 3
+
+
+if (Result1 < 7) {
+    console.log("Infelizmente voce não paso o curso ")
+} else {
+    console.log("Parabens, voce passo o curso")
+}
